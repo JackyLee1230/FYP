297a87371767bc2ba32f37ed70590df49d177b6a
Lee Chi Ho <78201931+JackyLee1230@users.noreply.github.com>
2023-09-21 15:31:56 +0800

    [Backend] Use DIgital Ocean MySQL

--- Backend/target/generated-sources/annotations/info/itzjacky/FYP/User/UserMapperImpl.java
+++ Backend/target/generated-sources/annotations/info/itzjacky/FYP/User/UserMapperImpl.java
@@ -8,7 +8,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-09-19T16:36:37+0800",
+    date = "2023-09-21T15:12:34+0800",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.6 (Azul Systems, Inc.)"
 )
 public class UserMapperImpl implements UserMapper {
