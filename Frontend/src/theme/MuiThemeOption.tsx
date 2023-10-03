bac226194096bec5a2a3eec1b3f398618066a25e
Siu Yuk Shing <79687526+Samsys1622@users.noreply.github.com>
2023-10-04 02:12:45 +0800

    feat: add custom theme option

---
+++ Frontend/src/theme/MuiThemeOption.tsx
@@ -0,0 +1,35 @@
+import { ThemeOptions } from '@mui/material/styles';
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#264653',
+    },
+    secondary: {
+      main: '#8884FF',
+    },
+    background: {
+      default: '#EFF7F6',
+      paper: '#E6E8E6',
+    },
+    error: {
+      main: '#d64550',
+    },
+    info: {
+      main: '#3c91e6',
+    },
+    warning: {
+      main: '#F0F757',
+    },
+    success: {
+      main: '#9CEC5B',
+    },
+    divider: '#8A948D',
+    text: {
+      primary: 'rgba(28,40,38,0.96)',
+      secondary: 'rgba(28,40,38,0.72)',
+      disabled: 'rgba(28,40,38,0.36)',
+    },
+  },
+};
