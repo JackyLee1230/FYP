700edb1fabe29a8ae652b2f424b9d55ca046c4ce
Jacky Lee <u3578552@connect.hku.hk>
2023-10-12 22:02:31 +0800

    [B] add constant for genre/platform

--- Backend/src/main/java/info/itzjacky/FYP/Game/GameGenre.java
+++ Backend/src/main/java/info/itzjacky/FYP/Game/GameGenre.java
@@ -1,30 +1,49 @@
 package info.itzjacky.FYP.Game;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public enum GameGenre {
-    ACTION_AND_ADVENTURE,
-    CLASSICS,
-    FAMILY_AND_KIDS,
-    INDIE,
-    PLATFORMER,
-    PUZZLE,
-    RPG,
-    SHOOTER,
-    SIMULATION,
-    SPORTS,
-    STRATEGY,
-    RHYTHM,
-    SURVIVAL,
-    HORROR,
-    MMO,
-    MOBA;
+    ACTION_AND_ADVENTURE(0),
+    CLASSICS(1),
+    FAMILY_AND_KIDS(2),
+    INDIE(3),
+    PLATFORMER(4),
+    PUZZLE(5),
+    RPG(6),
+    SHOOTER(7),
+    SIMULATION(8),
+    SPORTS(9),
+    STRATEGY(10),
+    RHYTHM(11),
+    SURVIVAL(12),
+    HORROR(13),
+    MMO(14),
+    MOBA(15);
 
-//    a function to return all the values of the enum
-    public static List<GameGenre> getAllGenres() {
-        return List.of(GameGenre.values());
+    private int id;
+
+    GameGenre(int id){
+        this.id = id;
     }
+
+    public int getID(){
+        return id;
+    }
+
+    //    a function to return all the values of the enum
+    public static List<Map<String, Integer>> getAllGenres() {
+        ArrayList<Map<String, Integer>> returnedList = new ArrayList<Map<String, Integer>>();
+        for (GameGenre g : GameGenre.values()) {
+            Map <String, Integer> tmp = new HashMap<>();
+            tmp.put(g.toString(), g.getID());
+            returnedList.add(tmp);
+        }
+        return returnedList;
+    }
+
 
 //    write a function that takes a string to find any genre that contains the string, and remove any underscore in the enum and ignore case
     public static List<GameGenre> getGenreFromString(String genre) {
--- Backend/src/main/java/info/itzjacky/FYP/Game/Platform.java
+++ Backend/src/main/java/info/itzjacky/FYP/Game/Platform.java
@@ -1,48 +1,60 @@
 package info.itzjacky.FYP.Game;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public enum Platform {
-    STEAM,
-    MAC,
-    GOG,
-    EPIC_GAMES,
-    ORIGIN,
-
-
-    NINTENDO_SWITCH,
-    NINTENDO_3DS,
-    NINTENDO_DS,
-    NINTENDO_WII,
-    NINTENDO_WII_U,
-    NINTENDO_GAMECUBE,
-    NINTENDO_64,
-    NINTENDO_GAMEBOY,
-    NINTENDO_GAMEBOY_ADVANCE,
-    NINTENDO_GAMEBOY_COLOR,
-    NINTENDO_GAME_AND_WATCH,
-    NINTENDO_ENTERTAINMENT_SYSTEM,
-    NINTENDO_SUPER_NINTENDO,
-
-
-    PS1,
-    PS2,
-    PS3,
-    PS4,
-    PS5,
-
-
-    XBOX,
-    XBOX_360,
-    XBOX_ONE,
-    XBOX_SERIES,
-
+    STEAM(0),
+    MAC(1),
+    GOG(2),
+    EPIC_GAMES(3),
+    ORIGIN(4),
+    NINTENDO_SWITCH(5),
+    NINTENDO_3DS(6),
+    NINTENDO_DS(7),
+    NINTENDO_WII(8),
+    NINTENDO_WII_U(9),
+    NINTENDO_GAMECUBE(10),
+    NINTENDO_64(11),
+    NINTENDO_GAMEBOY(12),
+    NINTENDO_GAMEBOY_ADVANCE(13),
+    NINTENDO_GAMEBOY_COLOR(14),
+    NINTENDO_GAME_AND_WATCH(15),
+    NINTENDO_ENTERTAINMENT_SYSTEM(16),
+    NINTENDO_SUPER_NINTENDO(17),
+    PS1(18),
+    PS2(19),
+    PS3(20),
+    PS4(21),
+    PS5(22),
+    XBOX(23),
+    XBOX_360(24),
+    XBOX_ONE(25),
+    XBOX_SERIES(26),
     ;
 
+    private int id;
+
+    Platform(int id){
+        this.id = id;
+    }
+
+    public int getID(){
+        return id;
+    }
+
 //    a function to return all the values of the enum
-    public static List<Platform> getAllPlatforms() {
-        return List.of(Platform.values());
+    public static List<Map<String, Integer>> getAllPlatforms() {
+        ArrayList<Map<String, Integer>> returnedList = new ArrayList<Map<String, Integer>>();
+        for (Platform p : Platform.values()) {
+            Map <String, Integer> tmp = new HashMap<>();
+            tmp.put(p.toString(), p.getID());
+            returnedList.add(tmp);
+        }
+        return returnedList;
     }
 
     public static List<Platform> getPlatformFromString(String platform) {
