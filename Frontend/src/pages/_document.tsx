6ea1e96d5c31ad98c831332c7940e3880c25d594
Siu Yuk Shing <79687526+Samsys1622@users.noreply.github.com>
2023-11-08 23:12:22 +0800

    fix: add meta for https

--- Frontend/src/pages/_document.tsx
+++ Frontend/src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </Head>
       <body>
         <Main />
         <NextScript />
