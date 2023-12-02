fcb100409759eccfbe1908dcbf7b6f7dbf7d4d31
Siu Yuk Shing <79687526+Samsys1622@users.noreply.github.com>
2023-12-02 17:39:20 +0800

    feat: update add review

--- Frontend/src/pages/new-review.tsx
+++ Frontend/src/pages/new-review.tsx
@@ -1,46 +1,52 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import 'tailwindcss/tailwind.css';
 import axios from 'axios';
+import Switch from '@mui/material/Switch';
+import { useAuthContext } from '@/context/AuthContext'
 
 const NEXT_PUBLIC_BACKEND_PATH_PREFIX  = process.env.NEXT_PUBLIC_BACKEND_PATH_PREFIX
 
 type ReviewData = {
-  name: string;
-  developer: string;
-  publisher: string;
+  gameId: number;
+  reviewerId: number;
   score: number;
-  releaseDate: string;
   comment: string;
+  recommended: boolean;
 };
 
 const AddNewReview = () => {
-  const router = useRouter();
+  const { user, token } = useAuthContext()
+  useEffect(() => {
+    if(user && token) {
+      setReviewerId(Number(user.id));
+    }
+  }, [user, token])
 
-  const [name, setName] = useState('');
-  const [developer, setDeveloper] = useState('');
-  const [publisher, setPublisher] = useState('');
+  const router = useRouter();
+  const [gameId, setGameId] = useState(0);
+  const [reviewerId, setReviewerId] = useState(0);
   const [score, setScore] = useState(0);
-  const [releaseDate, setReleaseDate] = useState('');
   const [comment, setComment] = useState('');
+  const [recommended, setRecommended] = useState(false);
+  const [error, setError] = useState('');
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const reviewData: ReviewData = {
-      name,
-      developer,
-      publisher,
-      score,
-      releaseDate,
-      comment
+      gameId: gameId,
+      reviewerId: reviewerId,
+      score: score,
+      comment: comment,
+      recommended: recommended,
     };
     console.debug(reviewData);
 
     try {
       const response = await axios.post(
-        `${NEXT_PUBLIC_BACKEND_PATH_PREFIX}api/game/addReview`, 
+        `${NEXT_PUBLIC_BACKEND_PATH_PREFIX}api/review/addReview`, 
         reviewData,
         {
           headers: {
@@ -53,25 +59,51 @@ const AddNewReview = () => {
       ); // 
       if (response.status === 200) {
         console.debug('Review added successfully');
-//      TODO: redirect to review page
-//      router.push('/reviews');
+        router.push(`/review/${response.data.id}`);
       } else {
-        console.debug('Failed to add review');
+        setError(response.data.message);
+        console.debug('Failed to add review', response);
       }
-    } catch (error) {
+    } catch (error: any) {
+        setError(error.response.data.message);
         console.error('Failed to add review');
     }
   };
 
+  if(reviewerId === 0) return (
+    <div className="mt-4">
+      <p>Please login to add a review</p>
+      <Link href="/login" className="px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700">
+          Login
+      </Link>
+    </div>
+  );
+
   return (
     <div className="mt-4">
       <form onSubmit={onSubmit} className="space-y-4">
-        <input value={developer} onChange={(e) => setDeveloper(e.target.value)} placeholder="Developer" required className="w-full p-2 border border-gray-300 rounded" />
-        <input value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" required className="w-full p-2 border border-gray-300 rounded" />
-        <input value={publisher} onChange={(e) => setPublisher(e.target.value)} placeholder="Publisher" required className="w-full p-2 border border-gray-300 rounded" />
-        <input value={score} onChange={(e) => setScore(Number(e.target.value))} placeholder="Score" type="number" min="0" max="100" required className="w-full p-2 border border-gray-300 rounded" />
-        <input value={releaseDate} onChange={(e) => setReleaseDate(e.target.value)} type="date" required className="w-full p-2 border border-gray-300 rounded" />
+        <div className="flex flex-col items-left space-x-2">
+          <label htmlFor="gameId">Game ID</label>
+          <input value={gameId} onChange={(e) => setGameId(Number(e.target.value))} placeholder="Game ID" type="number" min="0" required className="w-full p-2 border border-gray-300 rounded" />
+        </div>
+
+        <div className="flex flex-col items-left space-x-2">
+          <label htmlFor="reviewerId">{"Score (From 0 to 100)"} </label>
+          <input value={score} onChange={(e) => setScore(Number(e.target.value))} placeholder="Score" type="number" min="0" max="100" required className="w-full p-2 border border-gray-300 rounded" />
+        </div>
+
         <textarea value={comment} onChange={(e) => setComment(e.target.value)} placeholder="Comment" required className="w-full p-2 border border-gray-300 rounded h-20" />
+        <div className="flex items-center space-x-2">
+          <label htmlFor="recommended">Recommended</label>
+          <Switch
+            checked={recommended}
+            onChange={(e) => setRecommended(e.target.checked)}
+            inputProps={{ 'aria-label': 'controlled' }}
+          />
+        </div>
+        
+        {error && <p className="text-red-500">{error}</p>}
+        
         <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded">Submit Review</button>
       </form>
 
