36428884f38266cf07aac37a05f39e54d0646133
Jacky Lee <u3578552@connect.hku.hk>
2023-12-17 23:02:42 +0800

    feat: game recv for free msg

--- Frontend/src/utils/Definition.ts
+++ Frontend/src/utils/Definition.ts
@@ -1,8 +1,10 @@
-export const EarlyAccessDefinition = 
-    "An early access game is a game that is still under development and may undergo significant changes before its final release. The game may not meet the expected standards of quality or content at the current stage. The developers may or may not provide updates or revisions to the game in the future."
+export const EarlyAccessDefinition =
+  "An early access game is a game that is still under development and may undergo significant changes before its final release. The game may not meet the expected standards of quality or content at the current stage. The developers may or may not provide updates or revisions to the game in the future.";
 
 export const DLCDefinition = (baseGame: string | null | undefined) => {
-  return(
-    `This DLC/Expandsion requires the base game ${baseGame} to play!`
-  )
+  return `This DLC/Expandsion requires the base game ${baseGame} to play!`;
 };
+
+export const GameReceivedForFreeDefinition =
+  "Game received for free (Gifted/For Review)";
+
