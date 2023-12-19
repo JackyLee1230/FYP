9dc1d2e1c0ff3d2be1f65699f2501e17a1cc3110
Jacky Lee <u3578552@connect.hku.hk>
2023-12-19 11:20:29 +0800

    feat: update playTimeString func

--- Frontend/src/utils/Other.ts
+++ Frontend/src/utils/Other.ts
@@ -9,19 +9,19 @@ export function getEnumValues(e: EnumObject): string[] {
 // given an integer that represents minutes, if its greater than 1 day, show as Day + Hours, if its greater than hours, show Hours + Minutes, else show Minutes
 export function playTimeString(minutes: number): string {
   if (minutes <= 1) {
-    return "Unknown Playtime";
+    return "Unknown Playtime,";
   }
   if (minutes > 1440) {
     return `${Math.floor(minutes / 1440)} Days ${Math.floor(
       (minutes % 1440) / 60
-    )} Hours Played`;
+    )} Hours Played,`;
   } else if (minutes > 60) {
     if (minutes % 60 === 0) {
-      return `${Math.floor(minutes / 60)} Hours Played`;
+      return `${Math.floor(minutes / 60)} Hours Played,`;
     }
-    return `${Math.floor(minutes / 60)} Hours ${minutes % 60} Minutes Played`;
+    return `${Math.floor(minutes / 60)} Hours ${minutes % 60} Minutes Played,`;
   } else {
-    return `${minutes} Minutes Played`;
+    return `${minutes} Minutes Played,`;
   }
 }
 
