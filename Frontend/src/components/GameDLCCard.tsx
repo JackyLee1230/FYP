10a7a34430946588da081d1a7e68dedfedcd192b
Siu Yuk Shing <79687526+Samsys1622@users.noreply.github.com>
2023-12-20 23:38:59 +0800

    fix: set height to full

--- Frontend/src/components/GameDLCCard.tsx
+++ Frontend/src/components/GameDLCCard.tsx
@@ -54,7 +54,7 @@ function GameDLCCard({ game }: GameDLCCardProps) {
           </Typography>
         </Box>
       )}
-      <Box sx={{ display: 'flex', flexDirection: 'column', padding: "16px", width: "100%" }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', padding: "16px", width: "100%", height: "100%" }}>
         <Typography 
           variant="h6" 
           color="text.primary" 
