18dee3d9b415f60886dcaf39f8f35af84c278132
pakyimcheng <u3578494@connect.hku.hk>
2024-02-28 00:50:08 +0800

    Retrain & re-eval SA models with updated dataset
    Generated new dataset with more filtering on duplicate (review, sentiment) pairs in mid-Jan.
    
    Retrain the glove-cnn and tfidf-rf models with the new datasets, and then re-evaluate them.

--- NLP/dev-workspace/sa/tfidf-rf_2023-12-16/training_bashscript.sh
+++ NLP/dev-workspace/sa/tfidf-rf_2023-12-16/training_bashscript.sh
@@ -6,22 +6,22 @@ echo -e "\n\n"
 echo "TFIDF RF training bash script starts"
 echo -e "\n\n"
 
-python tfidf_rf_train_script.py 120 True
+python tfidf-rf_training_script.py 120 True
 echo -e "\n\n"
 
-python tfidf_rf_train_script.py 120 False
+python tfidf-rf_training_script.py 120 False
 echo -e "\n\n"
 
-python tfidf_rf_train_script.py 240 True
+python tfidf-rf_training_script.py 240 True
 echo -e "\n\n"
 
-python tfidf_rf_train_script.py 240 False
+python tfidf-rf_training_script.py 240 False
 echo -e "\n\n"
 
-python tfidf_rf_train_script.py 480 True
+python tfidf-rf_training_script.py 480 True
 echo -e "\n\n"
 
-python tfidf_rf_train_script.py 480 False
+python tfidf-rf_training_script.py 480 False
 echo -e "\n\n"
 
 echo -e "\n\n"
