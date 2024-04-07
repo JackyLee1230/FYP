cd6c05c94885d381bd30e95a43db1ea450d94f98
pakyimcheng <u3578494@connect.hku.hk>
2024-04-07 23:44:46 +0800

    update prompts & sample reviews to be consistent with main branch

--- NLP/dev-workspace/llm-rag/_prompts.py
+++ NLP/dev-workspace/llm-rag/_prompts.py
@@ -1,12 +1,12 @@
 # define prompt tempates for the LLM model
 
 ####################
-# Copy from main branch on 21/03/2024
+# Copy from main branch on 04/04/2024
 ####################
 
 
 SYSTEM_TEMPLATE = \
-'''You are reading reviews of a game to understand the characteristics of the game. Use the following pieces of context to answer user's question.'''
+'''You are a player reading reviews of a game to understand the characteristics of the game. Use the following pieces of context to answer user's question.'''
 
 
 # _check_spam
@@ -32,11 +32,14 @@ Output 'YES' if the review is a spam and 'NO' if the review is not a spam. Only 
 # gen_keywords_per_review
 KEYWORD_TEMPLATE_01 = \
 '''You are reading reviews of a game to understand the characteristics of the game. Extract the following aspect of the game from the reviews.
-The aspects are {aspects}. For each aspect, output a paragraph with less than 50 words. Then create a JSON with apsects name as key and the paragraph as value.
+The aspects are {aspects}. For each aspect, output a paragraph with less than 50 words. Then create a JSON with aspects name as key and the paragraph as value.
 {output_format}
 Only output the JSON. Do NOT output other text.
 
+The reviews are as follows:
+\'\'\'
 {summaries}
+\'\'\'
 
 If you don't know the answer, output only \"NA\". Do NOT try to make up an answer. Do NOT output other text.'''
 
@@ -44,12 +47,26 @@ OUTPUT_FORMAT_TEMPATE = \
 '''Output the JSON as a single line with no spaces between the key, value pairs. For example, if the aspects are {aspects_list_01}, the JSON should be: {output_json_template}'''
 
 QUESTION_TEMPLATE_01 = \
-'''Extract the the following aspect of the game from the reviews. The aspect '''
+'''Extract the following aspect of the game from the reviews. The aspect '''
+
+
+ASPECT_SENTIMENT_TEMPLATE = \
+'''You are reading reviews of a game to understand the characteristics of the game. Determine whether the reviews of each aspect are positive, or negative.
+The aspects are {aspects}. For each aspect, output \'positive\' if the review is positive, and \'negative\' if the review is negative. Output \'NA\' if there is no review for an aspect. Only output the decision. Do NOT output other text. Then create a JSON with aspects name as key and sentiment as value.
+{output_format}
+Do NOT output other text.
+
+The reviews are as follows. The reviews are wrapped by three consecutive apostrophes
+\'\'\'
+{context}
+\'\'\'
+
+Do NOT try to make up an answer. Do NOT output other text.'''
 
 
 
 KEYWORD_TEMPLATE_02 = \
-'''Extract the following aspects of the game from the reviews, and providwe a list of keywords, each of max length 5 words, for each aspect. The aspects are: {aspects}. Output a JSON with each of the aspects as key, and the list of keywords as the value. Only output the JSON. Do NOT output other text.
+'''Extract the following aspects of the game from the reviews, and provide a list of keywords, each of max length 5 words, for each aspect. The aspects are: {aspects}. Output a JSON with each of the aspects as key, and the list of keywords as the value. Only output the JSON. Do NOT output other text.
 
 The context is wrapped by three consecutive apostrophes. The context is as follows:
 \'\'\'
--- NLP/dev-workspace/llm-rag/_sample_reviews.py
+++ NLP/dev-workspace/llm-rag/_sample_reviews.py
@@ -157,3 +157,59 @@ That brings me nicely to exploration. Who the hell at Bethesda played this and t
 I can't begin to state how much of a better design decision it would have been for Starfield to have only a few planets that were handcrafted without invisible walls or random generation, or at least, that kept to a minimum. But they sold that away for the "1,000 planets" marketing gimmick. Beautiful handcrafted worlds are the bread and butter of Bethesda's games. It's why we love them and more importantly, why we put up with the bugs and the other misgivings. Without that, what does the game have? Nothing. And that's where we're at with Starfield.
 
 In general, I feel that Bethesda may have just lost it. Other game companies have put out far superior titles with features that are better than in Starfield, or they're actually present in the first place. It's like they're sitting on a sugar high from the Skyrim days (and even then, the game has actually gone backwards in several respects). They need to wake up and move on, because everyone else has. They also talk about how this game is their 'passion project' that they've wanted to make since Morrowind. If this is their passion project, you should be very, very concerned about what comes next.'''
+
+critic_review_01 = \
+'''Cyberpunk 2077
+Review by John Tucker, January 3, 2021
+
+Over the last few weeks, one question has run through my head many, many times: how do I actually review Cyberpunk 2077? It was one of the most highly-anticipated releases of the year, and although people were frustrated when its release got delayed more than once, I think they understood that COVID was causing trouble for everyone and hopeful that a delay would mean a very polished game. But then the release approached and news came out that, despite earlier promises that developers wouldn’t be required to deal with “crunch time,” they had to deal with it for more than a year.
+
+When it finally released, the console versions were so buggy as to be unplayable, and both digital and physical retailers expanded their refund policies to accommodate anyone who wanted to return the game. Here at RPGFan, one reviewer was unable to finish even the game’s prologue on PC before the game crashed permanently. With the copy I personally purchased on PC, I had better luck and was able to play all the way to the end credits with only minor glitches.
+
+So, do I factor those elements into my score because they’re a part of the game’s ecosystem, or do I leave them out unless they impact my particular experience? In the end, I hope to walk the tightrope and find a balance between the two.
+
+In Cyberpunk 2077, you play a character known only as V who takes a page from the Dragon Age: Origins book and starts in one of three places of your choice. After a short prologue, all three stories converge in the same area, but you have dialogue options related to your specific backstory throughout the game. The main story deals with a question at the heart of the cyberpunk genre: what does it mean to be human? It’s a difficult question, and I don’t know that this game attempts to give a definitive answer so much as it tells one person’s struggle to decide what it means to them and what they feel is justifiable in the pursuit of holding onto their humanity and defining a legacy for themselves.
+
+I’m told that the main story could be completed in only 20 hours or so, but those missions are just the tip of the iceberg in terms of what this game has to offer. I played the final story mission at about the 70 hour mark so that I could write a review, but there may still be that amount of content again for me to play. There are a lot of side quests to go on with characters like Keanu’s, and you don’t want to miss that, because it’s great stuff. It’s worth pointing out a number of these characters are middle-aged or older women, which is pretty unusual and cool. The nature of tech in the Cyberpunk universe means they don’t look as old as their birthdate says they are, but they’re not 20 and they don’t act like they are.
+
+
+The world map is jam packed with side gigs from taking out some very bad people by any means necessary to sneaky heists where getting caught means failure. Some are silly. Others will stick with you long after you’re done, wondering if you made the right choice not as a character in a game that will likely be unaffected by that side mission, but as the player who made the choice. What this game offers in that respect is truly outstanding.
+
+Assuming you can play the game at all, its gameplay really shines. When given the option, I tend to play games as a sneaky sniper type, popping my head out to zap an enemy without being noticed, and this game definitely allowed for that just as well as it allowed for those who like to break down the front door and start tossing grenades. In one infiltration mission, I stopped and counted at least six possible ways I could have gotten to my target, and at that point, I didn’t even have the leg enhancements that gave me a fifteen foot vertical leap. There were only two points in the game where I was trapped in a small space with no real cover and no choice but to fight, and although it took a couple of tries, I was able to get through them without wanting to throw my controller. 
+
+There are multiple types of guns on offer from pistols to shotguns, but also guns that ricochet and guns that use smart targeting to home in on enemies. There are also a plethora of silent melee options, from katanas to hammers to… a large flesh-colored club you won’t want your mom to see you wielding. But you don’t have to do everything with your hands — in this world, everybody’s got a variety of hardware built into their body somewhere, and you can use yours to hack theirs. You start with simple options, but by the end of the game, if you’ve bought the upgrades, you’ll be locking your enemies in place, blinding them, and non-lethally zapping them into unconsciousness two at a time all while getting critical headshots with your pistol. For me, normal difficulty was just enough to make me feel great when I did cool things while still presenting plenty of evidence that I was not unstoppable.
+
+I used a PS4 controller on my PC, and the controls were mostly great when it counted. I had no trouble running, hacking, and shooting, and when I drove a vehicle, my R2 “gas pedal” allowed me to choose my speed. As I got my hands on a few vehicles, I noticed how differently they handled, which I appreciate. I started the game as a Nomad, and I oversteered horribly when driving V’s original car, but the more Buick-like car I got later was easy to handle, and I loved weaving motorcycles through traffic. Sadly, I had to use a wire for my PS4 controller — I have the game via GoG Galaxy, which only appears to support wireless play for Xbox controllers — but I was playing on a laptop, so the wire wasn’t a real obstacle.
+
+
+The controls aren’t perfect, though. I tried driving with a keyboard at one point, and the binary nature of those controls didn’t work for me at all. In contrast, I always felt like the menu screens were clearly designed with a mouse in mind, as they present you with a sort of mouse cursor and the d-pad often doesn’t move around as you might hope. Still, the controls are good enough and flexible enough that keyboard or controller fans are all likely to find something they can work with.
+
+Cyberpunk 2077‘s presentation is top-notch as well, with excellent graphics, music, and voice acting on display from start to finish… mostly. Although the game only crashed on me once, this is the area where I ran into glitches. There were occasional NPCs clipping through objects or each other, but that didn’t happen frequently or bother me much. More frustratingly, though, I ran into quite a number of occasions where interactable objects stopped being highlighted in the game world, or when I pushed the button to hack them. A few times, one bar of music from an action scene kept looping after the scene was over until I restarted the game. None of this was an every few minutes kind of issue — more like every couple of hours.
+
+As long as things aren’t glitchy, it’s extremely interesting to walk and drive around Night City seeing everything that’s on display. The various sectors of the city have their similarities, but they are mostly distinct, from the types of buildings and roads to the people walking around. Head to the edges of town and you can find the mansions of the elite or the nasty, nasty garbage dump and oil field. The business district is shiny and clean, without the vending machines present all over the residential areas. I finished the game’s story yesterday, but I’ll definitely be back to find more of what I haven’t seen yet.
+
+Despite my praise for Cyberpunk 2077‘s overall aesthetic, I would be wrong not to point out a flaw that shows up repeatedly. One of the often-seen in-game billboards features a transphobic joke, and there are several other design elements that would be generously described as unfriendly to transgender people. When these issues were pointed out to developer CD Projekt RED, their response was to double down by mocking those same people. You might think “OK, that’s not a good call, but why does this specific game matter in that fight?” But it’s important to know that transhumanism — the idea that a human body as it is when one is born is only a starting point, and that technology can (and should) be used to change that body as needed — is a crucial element of the cyberpunk genre. This game should have been the AAA title that was the most friendly to the transgender community rather than a bad example. Bringing up these bad choices in the design and PR isn’t political correctness: it’s calling out the game for failing to correctly reflect its setting, like it would be if a medieval simulation game included a character with a gun.
+
+With all of this in mind, Cyberpunk 2077 ends up being one of those games that can be frustrating to love. There are good design elements all over, from a menu option disabling licensed music for streaming to quests that completely change based on whether you choose to go in guns blazing or stay hidden and make sense both ways. The main and side quests are all packed full of great story, the characters are fun to get to know… and yet despite all of the effort that went into this game, we also see the bad design choices I discussed above and an unplayable console version. If you can take the places where it’s a bad example and remember to do better in your own life, and maybe wait until it’s been patched into working, there’s an amazing game here that you can be glad you experienced.
+
+Pros
+Great story, gameplay, and characters. Hanging out with Keanu is a joy.
+
+Cons
+Unplayably glitchy on a console and on some PCs, bad choices in transgender design that are relevant to the genre.
+
+Bottom Line
+Cyberpunk 2077 is a heck of a game, if you can manage to play it.
+
+Graphics
+85
+Sound
+90
+Gameplay
+90
+Control
+80
+Story
+95
+Overall Score
+70'''
